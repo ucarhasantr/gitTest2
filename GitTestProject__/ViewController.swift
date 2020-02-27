7a2fd34672cf4f11057f1f2279292ff053d66baf
Hasan UCAR <>
2020-02-27 10:01:24 +0300

    ikinci değişiklik

--- GitTestProject__/ViewController.swift
+++ GitTestProject__/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hhshshshsh")
+        print("ookkkkk")
+        print("adjad uoıaduıoa")
        
     }
 
