f4b984332b4294f301cde95b57cab8aa94833b2d
Hasan UCAR <>
2020-02-27 09:59:04 +0300

    ilk değişiklik

---
+++ GitTestProject__/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitTestProject__
+//
+//  Created by Hasan UCAR on 27.02.2020.
+//  Copyright © 2020 Hasan UCAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hhshshshsh")
+       
+    }
+
+
+}
+
